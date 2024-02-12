c393119ee01a4cfd6aeedd8328104cd9988895b3
WaniaJP <114880028+WaniaJP@users.noreply.github.com>
2024-02-12 23:43:10 +0100

    Etape 3 : Ajout de la connexion

--- topic/src/redpanda/consumer.js
+++ topic/src/redpanda/consumer.js
@@ -1,5 +1,6 @@
 import {Kafka, logLevel} from "kafkajs";
 import {getLocalBroker} from "../config/config.js";
+import { createClient } from 'redis';
 
 const isLocalBroker = getLocalBroker()
 const redpanda = new Kafka({
@@ -9,18 +10,34 @@ const redpanda = new Kafka({
 });
 
 const consumer = redpanda.consumer({ groupId: '...' });
+const url = "redis://myredis";
+const pwd = "redispwd";
+
+let  client;
 export const connexion = async (topic) => {
     await consumer.connect();
-    await consumer.subscribe({topics : [topic], fromBeginning: true});
+    await consumer.subscribe({topics : [topic]});
+    //await consumer.subscribe({topics : [topic], fromBeginning: true});
+    client = await getClient();
+    await client.connect().then(()=> {
+        console.log('yes ! ')
+    })
+
 
     await consumer.run({
         eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
             let messageJson = JSON.parse(message.value.toString());
+
+            let motsInMessage = messageJson.message.split(" ");
+            console.log(motsInMessage);
+
+            await setValue(motsInMessage);
+            /*
             console.log({
                 date: getDateFromTimestamp(message.timestamp.toString()),
                 utilisateur: messageJson.user.toString(),
-                message: messageJson.message.toString(),
-            })
+                message: messageJson.message,
+            })*/
         },
     })
 }
@@ -34,4 +51,24 @@ export const disconnect = async () => {
 
 export function getDateFromTimestamp(timestamp) {
     return new Date(parseInt(timestamp)).toLocaleString();
+}
+
+export const getClient = async() => {
+    return createClient({
+        host : url,
+        username : 'default',
+        password: pwd,
+        port : "6379"
+    });
+}
+
+export const setValue = async(words) => {
+
+    for (const word of words){
+
+        if( await client.exists(word) !== 1){
+            await client.set(word, 0)
+        }
+        await client.incr(word);
+    }
 }
